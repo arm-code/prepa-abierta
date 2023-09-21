a7e37a5c8018f015500e00e5265b86bc96290d18
Alexis Romero MEndoza <al162917@alumnos.uacj.mx>
2023-09-21 14:10:27 -0600

    cambiando ruta de la imagen por una web

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ export function Title() {
   return (
     <div className="flex flex-col justify-center items-center p-4 text-center">
       <img
-        src="src/assets/img/spayt.png"
+        src="https://spaytchihuahua.gob.mx/images/logoSPAyT.png"
         alt="Logo del spayt"
         className="w-64 h-auto"
       />
