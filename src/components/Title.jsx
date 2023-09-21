b2015bf8eb58f776f3c19d11b802a2fdc176723a
Alexis Romero Mendoza <64325158+arm-code@users.noreply.github.com>
2023-09-20 18:54:19 -0600

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ export function Title() {
   return (
     <div className="flex flex-col justify-center items-center p-4 text-center">
       <img
-        src="src/img/spayt.png"
+        src="src/img/spayt.jpg"
         alt="Logo del spayt"
         className="w-64 h-auto"
       />
